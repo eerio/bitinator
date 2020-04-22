e121bbe945ebbe69ea317cfc5b135ac381b1a7d0
Pawel Balawender <eerio@debian-vm.pl>
2020-04-22 18:21:19 +0200

    test new htaccess

--- test-dns.py
+++ test-dns.py
@@ -4,34 +4,27 @@ from requests import get
 base = 'bitinator.net/'
 http = 'http://'
 https = 'https://'
-sub = 'pawel.'
-pat = 'agawa'
+sub = 'pawel.'  # non-existent subdomain
+pat = 'agawa'  # non-existent path
 www = 'www.'
+tod = 'todo.'
 
-red = 303
 
-# check if naked resolves to https + www
-for resp in (get(http + base), get(https + base)):
-    print(resp.history)
+for schema in (http, https):
+    # base --303--> https+www+base
+    resp = get(schema + base)
     assert len(resp.history) == 1
-    assert resp.history[0].status_code == red
+    assert resp.history[0].status_code == 303
     assert resp.url == https + www + base
 
-print(get("http://bitinator.net").history)
-"https://bitinator.net"
+# http+www+base --303--> https
+resp = get(http + www + base)
+assert len(resp.history) == 1
+assert resp.history[0].status_code == 303
+assert resp.url == https + www + base
 
-"http://www.bitinator.net"
-"https://www.bitinator.net"
-
-"http://www.bitinator.net"
-"https://www.bitinator.net"
-
-"http://www.bitinator.net/pawel"
-"https://www.bitinator.net/pawel"
-
-"http://pawel.bitinator.net"
-"https://pawel.bitinator.net"
-"http://pawel.bitinator.net/agawa"
-"https://pawel.bitinator.net/agawa"
-
+# https+www+base --> https+www+base
+resp = get(https + www + base)
+assert len(resp.history) == 0
+assert resp.url == https + www + base
 
